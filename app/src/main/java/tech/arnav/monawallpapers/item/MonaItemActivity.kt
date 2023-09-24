e89573741624210ec5f1602943433f8532e2912d
Arnav Gupta <championswimmer@gmail.com>
2023-09-24 01:47:27 -0400

    add viewer
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

--- app/src/main/java/tech/arnav/monawallpapers/item/MonaItemActivity.kt
+++ app/src/main/java/tech/arnav/monawallpapers/item/MonaItemActivity.kt
@@ -1,24 +1,39 @@
 package tech.arnav.monawallpapers.item
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import kotlinx.serialization.decodeFromString
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
 import kotlinx.serialization.json.Json
-import tech.arnav.monawallpapers.R
 import tech.arnav.monawallpapers.databinding.ActivityMonaItemBinding
 import tech.arnav.monawallpapers.http.MonaDatum
 
 class MonaItemActivity : AppCompatActivity() {
     val TAG = "MonaItemActivity"
+    lateinit var _binding: ActivityMonaItemBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        with(ActivityMonaItemBinding.inflate(layoutInflater)) {
-            setContentView(root)
-        }
+        _binding = ActivityMonaItemBinding.inflate(layoutInflater)
+        setContentView(_binding.root)
 
         val data = intent.getStringExtra("data")
         val monaData = Json.decodeFromString<MonaDatum>(data!!)
 
+        _binding.tvMonaArtist.text = monaData.artist
+        _binding.tvMonaTitle.text = monaData.title
+
+        // Load webm with exoplayer
+        val player = ExoPlayer.Builder(this).build()
+        _binding.playerView.player = player
+
+        // Build the media item.
+        val mediaItem: MediaItem = MediaItem.fromUri(monaData.webmURL)
+        // Set the media item to be played.
+        player.setMediaItem(mediaItem)
+        // Prepare the player.
+        player.prepare()
+        player.repeatMode = ExoPlayer.REPEAT_MODE_ALL
+        // Start the playback.
+        player.play()
     }
 }
